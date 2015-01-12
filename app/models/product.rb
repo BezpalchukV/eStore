e19ee98139a2741827c9151a2446e1fefcfdc032
Bezpalchuk V <monferat@ukr.net>
2015-01-12 17:59:59 +0200

    Change aws S3

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,14 +9,15 @@ class Product < ActiveRecord::Base
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.00 }
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
-  has_attached_file :photo, styles: {
-                               thumb: '100x100>',
-                               square: '200x200#',
-                               medium: '300x300>'
-                           }
+  has_attached_file :photo
+  # , styles: {
+  #                              thumb: '100x100>',
+  #                              square: '200x200#',
+  #                              medium: '300x300>'
+  #                          }
 
   # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/jpg', 'image/png'] #/\Aimage\/.*\Z/
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   before_destroy :ensure_not_referenced_by_any_order_item
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,10 +41,18 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
       :storage => :s3,
-      :bucket => ENV['S3_BUCKET_NAME'],
+      # :s3_host_name => 'REMOVE_THIS_LINE_IF_UNNECESSARY',
       :s3_credentials => {
-          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+          :bucket => 'kolenqur'
       }
   }
+
+  # config.paperclip_defaults = {
+  #     :storage => :s3,
+  #     :bucket => ENV['S3_BUCKET_NAME'],
+  #     :s3_credentials => {
+  #         :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #     }
+  # }
 end
